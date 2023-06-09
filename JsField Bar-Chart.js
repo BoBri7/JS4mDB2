b2d2f515c40326550c025d00b7f8b28d4999f8b2
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-06-09 20:59:30 +0200

    Update JsField Bar-Chart.js

--- JsField Bar-Chart.js
+++ JsField Bar-Chart.js
@@ -2,23 +2,21 @@
 *
 *
 *
-*
-*
 */
 function barCh(i,n,N,x){
   function a(x){return x<0 ?-x :x}
   if(i==null) return "barCh( i=index  n= N =#dekad del#[bmX])"
   const m=n*N
   var s=" ".repeat(n-1)
-   s=("|"+s).repeat(N)+"|"+(s+"|").repeat(N)
-   if(i==0) return s
-   s=s.split("");
+  s=("|"+s).repeat(N)+"|"+(s+"|").repeat(N)
+  if(i==0) return s
+  s=s.split("");
   for(let ii=1;ii< a(i);ii++){
-     if(a(i)<m)   s[m+(i>0 ?ii :-ii)]= i>0 ?" " : " "
+    if(a(i)<m) s[m+(i>0 ?ii :-ii)]= i>0 ?"<" : ">"
   }
   s[0]="|"
   s[m]="|";
   s[m*2]="|"
-  return (i<-m ?"-" :" ") +s.join("") + (i>m ?"+" :" ")+i
+  return (i<-m ?"-" :" ") +s.join("") + (i>m ?"+" :"")+i
 }  //=== end ===
 //log(barCh(  12,10,2,0))
