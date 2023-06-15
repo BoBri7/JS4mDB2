fe13cde36a048805d7d351e61f49e1e7db790438
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-06-15 13:14:44 +0200

    Update JSfield-functions gh2.js
    Corr round() /devimalke 00/poravnava!

--- JSfield-functions gh2.js
+++ JSfield-functions gh2.js
@@ -5,8 +5,11 @@
 * m(d)   R: moment(d)
 * df(dv,df="DD.MM.YY HH:mm")
 * round(xv,n) R: round
+* N($)        R: Number
+* S(x)        R: String
 */
 function jsff(){return "java script field fx2"}
+
 // field(fn)
 function f(fn){ return field(fn)}
 
@@ -27,13 +30,21 @@ function df(dv,df){//if(df==null)df="DD.MM.YY HH:mm"
    : moment(dv).format(df!=null?df:"DD.MM.YY HH:mm")
 }
 // round(#,d)
-function round(xv,n){ n=n!=null ?n :1 //def=1
+/*function round(xv,n){ n=n!=null ?n :1 //def=1
   n=n>0
     ? 1+"0".repeat(n)
     : 1/(1+"0".repeat(-n))
   return Math.round(xv*n)/n
-}
-// padLeftRight
+}*/
+function N(x){return Number(x.replace(",","."))}
+function S(x){return String(x)}
+function round(xv,n){ if(n==null) n =1
+  var o=1+"0".repeat(Math.abs(n))
+  if( n < 0)  return Math.round(xv/o)*o
+  else { xv = String(xv+".0").split(".")
+   return xv[0]+","+(xv[1]+"0".repeat(n)).slice(0,n)
+}}
+// pad Right Left
 function prl(s,n){ n=n!=null ?n :15*(typeof(v)!="string"?-1:1)
  s= (n>0 ?s:"")+" ".repeat(Math.abs(n))+(n>0 ?"" :s)
  return n>0
