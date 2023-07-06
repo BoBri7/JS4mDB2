48af46df638d9dfc6e21a41ac621659b4da1fbe4
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-07-06 07:16:30 +0200

    Update WRAP2LINE.js

--- WRAP2LINE.js
+++ WRAP2LINE.js
@@ -1,21 +1,34 @@
 /*  WRAP to LINES
+* wT2l(txt, hed, ll=25, HedDl=":")
+*  = multiLine Text >> lines
 * wt2l(txt, hed, ll=25, HedDl=":")
 *   = prepiše text v vrstice dolžine ll
 * wl2l(lfn, hd, ll)
 *   = prepiše ListField v vrstice 
 */
-function wrap2line(){
- let o="> wt2l(txt$,hd$#,Llen#=25 , dl$=:"
- o+= "\n> wl2l(lfn$, hd, ll#=25)"
- return o
+function h() { return
+" >wT2l(txt, hed, ll=25, Dl=":" = multiLine Text >> lines
+ >wt2l(txt, hed, ll=25, Dl=":") = prepiše text v vrstice dolžine ll
+ >wl2l(lfn, hd, ll)             = prepiše ListField v vrstice 
+"
 }
-// wrap text 2 lines
+
+// wrap MultiLineText
+function wT2l(txt, hd, ll, dl){txt=txt.split("\n")
+  for(let ii=0;ii<txt.length ;ii++){
+    if(ii==1) hd=hd.length
+    txt[ii]=wt2l(txt[ii], hd, ll, "")
+  }
+  return txt.join("\n")
+}
+
+// wrap text 2 line
 function wt2l(txt, hd, ll, dl){ if(ll==null)ll=25
   const d=" "
   var o="", l="t:"+d.repeat(hd-2)
-  if( typeof(hd)=="string" ){
-     l=hd+(dl!=null ?dl:":"); hd=d.repeat(hd.length)
-  }else hd = d.repeat(hd)
+  if( typeof(hd)=="number" ) hd = d.repeat(hd)
+  l=hd+(dl!=null ?dl :":");
+  hd=d.repeat(hd.length) //naslednje vrstice!
   txt=txt.split(" ")
   for(var ii=0;ii<txt.length;ii++){ ti=txt[ii]
    if(  l.length + ti.length /1.7 < ll ) l+= d
@@ -24,7 +37,7 @@ function wt2l(txt, hd, ll, dl){ if(ll==null)ll=25
   }
  return o+l
 }
-//== wrap list 2 lines
+//== wrap listField 2 lines
 function wl2l(lfn, hd, ll){ll=ll!=null ?ll :25
   var o=((hd!=null ?hd :lfn+":")+" ".repeat(ll)).slice(0,ll)
   o+=":"
